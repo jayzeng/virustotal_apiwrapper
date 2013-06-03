b782d8a2aef38899dd34afeae679f8e4c0f18e39
Jay Zeng <jayzeng@jay-zeng.com>
2013-06-03 18:10:06 +0000

    * add a missing param

--- Src/VirusTotal/Comment.php
+++ Src/VirusTotal/Comment.php
@@ -5,9 +5,10 @@ class Comment extends ApiBase
 {
     /**
      * @see https://www.virustotal.com/en/documentation/public-api/#making-comments
+     * @param string $resource   resource id
      * @param string $comment    the actual review, you can tag it using the "#" twitter-like syntax (e.g. #disinfection #zbot) and reference users using the "@" syntax (e.g. @VirusTotalTeam).
      */
-    public function addComment($comment) {
+    public function addComment($resource, $comment) {
         $data = $this->makePostRequest('comments/put', array(
                         'resource' => $resource,
                         'comment'  => $comment,
